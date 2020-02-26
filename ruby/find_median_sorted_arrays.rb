26713265c23c9d7b75f4727b0463ccb67f3363a1
phil <phil.roth077@gmail.com>
2020-02-26 18:17:49 -0500

    median sort refactor

--- ruby/find_median_sorted_arrays.rb
+++ ruby/find_median_sorted_arrays.rb
@@ -6,9 +6,9 @@ require 'pry'
 
 def find_median_sorted_arrays(nums1, nums2)
   total = nums1.length + nums2.length
-  arr = []
   i = j = k = 0
   # * Merge the arrays
+  arr = []
   while i < total
     if nums1[j] == nil
       arr << nums2[k]
@@ -22,11 +22,11 @@ def find_median_sorted_arrays(nums1, nums2)
     end
       i+=1
   end
-
+  
   while arr.length > 2
     arr = arr.slice(1, arr.length-2)
   end
-
+  
   if arr.length > 1 
     return (arr[0] + arr[1])/2.0
   else
@@ -34,9 +34,20 @@ def find_median_sorted_arrays(nums1, nums2)
   end
 end
 
+def find_median_sorted_arrays_2(nums1, nums2)
+  arr = (nums1 + nums2).sort()
+  total = arr.length
+  mid = ((total - 1).to_f / 2.0).floor
+  
+  if total.odd?
+    arr[mid].to_f
+  else
+    (arr[mid] + arr[mid+1]).to_f / 2.0
+  end
+end
 
-p find_median_sorted_arrays([1,3], [2]) # * == 2.0
-p find_median_sorted_arrays([1,2], [3,4]) # * == 2.5
-p find_median_sorted_arrays([1,2,4,6,7], [2,4,5,8]) # * == 4.0
-p find_median_sorted_arrays([1,3], []) # * == 2.0
+p find_median_sorted_arrays_2([1,3], [2]) # * == 2.0
+p find_median_sorted_arrays_2([1,2], [3,4]) # * == 2.5
+p find_median_sorted_arrays_2([1,2,4,6,7], [2,4,5,8]) # * == 4.0
+p find_median_sorted_arrays_2([1,3], []) # * == 2.0
 # The median is (2 + 3)/2 = 2.5
