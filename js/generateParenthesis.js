1fa14281368e04881f9e87c00f352011a5ff18ba
Philip Roth <50246817+rothberry@users.noreply.github.com>
2020-03-10 13:46:47 -0500

    flip n invert

---
+++ js/generateParenthesis.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ * Given n pairs of parentheses,
+ * write a function to generate all combinations of well-formed parentheses.
+ */
+const generateParenthesis = n => {
+  // Start by creating an array with n open and n closed
+  // Starting pivot is n and n+1
+  let parArr = []
+  let i = 0
+  while (i < n * 2) {
+    if (i < n) {
+      parArr.push("(")
+    } else {
+      parArr.push(")")
+    }
+    i++
+  }
+  let output = [parArr.join("")]
+  let j = 0
+  let k = n
+  while (k < n * 2) {
+    // swap k and k+1
+    // const cur = parArr[k]
+    // const next = parArr[k + 1]
+    // parArr.splice(k, 2, next, cur)
+    // ;[parArr[k], parArr[k + 1]] = [parArr[k + 1], parArr[k]]
+    let b = parArr[k + 1]
+    parArr[k + 1] = parArr[k]
+    parArr[k] = b
+    console.log(parArr)
+    output.push(parArr.join(""))
+    k++
+  }
+
+  return output
+}
+
+console.log(generateParenthesis(3))
+// [
+//   "((()))",
+//   "(()())",
+//   "(())()",
+//   "()(())",
+//   "()()()"
+// ]
--- ruby/binary_gap.rb
+++ ruby/binary_gap.rb
@@ -1,5 +1,10 @@
 # @param {Integer} n
 # @return {Integer}
+# In the binary representation of 22, there are three ones, 
+# and two consecutive pairs of 1's.
+# The first consecutive pair of 1's have distance 2.
+# The second consecutive pair of 1's have distance 1.
+# The answer is the largest of these two distances, which is 2.
 
 require 'pry'
 
@@ -23,13 +28,6 @@ def binary_gap(n)
 end
 
 p binary_gap(22)  == 2
-# output: 2
 p binary_gap(5)   == 2
 p binary_gap(6)   == 1
 p binary_gap(8)   == 0
-# 22 in binary is 0b10110.
-# In the binary representation of 22, there are three ones, 
-# and two consecutive pairs of 1's.
-# The first consecutive pair of 1's have distance 2.
-# The second consecutive pair of 1's have distance 1.
-# The answer is the largest of these two distances, which is 2.
