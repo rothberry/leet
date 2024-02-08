c7fc1cc83e63ce8cf9b9c78936f722ea0f8e2772
rothberry <phil.roth077@gmail.com>
2024-02-08 15:30:37 -0800

    more water

--- python/most_water.py
+++ python/most_water.py
@@ -13,8 +13,38 @@ from ipdb import set_trace
 from py_term_helpers import top_wrap, kv_print
 
 
-def max_area(height):
-    pass
+def max_area(heights):
+    # Two pointer
+    # start with current max and right/left pointers
+    # loop until the pointers cross
+    # for each
+    #   calc the area by:
+    #       width = right - left
+    #       height = min(height[right], height[left])
+    #
+    cur_max, l, r = 0, 0, len(heights) - 1
+    while l < r:
+        width = r - l
+        height = min(heights[l], heights[r])
+        cur_area = width * height
+        cur_max = max(cur_area, cur_max)
+
+        if heights[r] >= heights[l]:
+            l += 1
+        else:
+            r -= 1
+        kv_print((width, height, cur_area, cur_max))
+    print(cur_max)
+    return cur_max
 
 
 top_wrap("MOST WATER")
+
+height1 = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+# Output: 49
+
+height2 = [1, 1]
+# Output: 1
+
+max_area(height1)
+max_area(height2)
