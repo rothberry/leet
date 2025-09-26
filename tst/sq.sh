f8195426ffe278f1ce906292391551a73fd2a9c0
phil <phil.roth077@gmail.com>
2025-09-26 13:53:04 -0400

    t87yi

---
+++ tst/sq.sh
@@ -0,0 +1,15 @@
+echo $PWD
+uuid=$(uuidgen)
+echo $uuid
+
+gcp(){
+    git checkout gsgb
+    git pull origin master
+    echo $uuid >> tst/test.txt
+    git add .
+    git commit -m "$uuid"
+    git push origin gsgb
+    git checkout master
+}
+
+gcp
