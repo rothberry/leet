74a984c8926cfd0969f7b4755280e49367d9a338
phil <phil.roth077@gmail.com>
2025-09-26 13:56:07 -0400

    6F86746D-4EAB-449A-BBE8-235D30943F48

--- tst/sq.sh
+++ tst/sq.sh
@@ -7,7 +7,7 @@ echo $uuid >> tst/test.txt
 gcp(){
     git checkout gsgb
     git pull origin master
-    git add -A tst/test.txt
+    git add .
     git commit -m "$uuid"
     git push origin gsgb
     git checkout master
